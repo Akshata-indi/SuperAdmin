4bb324c5e79f45fcc4556cddfbbdd267e9c9df24
iramsaba-m <mirjankar.iramsaba@infokalash.com>
2023-11-16 11:11:53 +0530

    form modal changes

--- super_admin/src/components/design/form_modal/FormModal.js
+++ super_admin/src/components/design/form_modal/FormModal.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Modal = ({ isOpen, onClose, config, children }) => {
+const FormModal = ({ isOpen, onClose, config, children, backdropClass }) => {
   return (
     <div>
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
-          {/* ... Modal content ... */}
+          {/* Semi-transparent overlay for background */}
+          <div className={`modal-overlay fixed inset-0 ${backdropClass}`}></div>
+
+          {/* Modal container */}
           <div className="modal-container bg-white p-10 rounded-lg shadow-md relative z-10">
             <h2 className="text-xl font-semibold mb-2">{config.title}</h2>
             <p className="text-gray-600">{config.content}</p>
@@ -30,4 +33,4 @@ const Modal = ({ isOpen, onClose, config, children }) => {
   );
 };
 
-export default Modal;
+export default FormModal;

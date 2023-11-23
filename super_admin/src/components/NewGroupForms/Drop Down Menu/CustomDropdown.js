518bba576617eb5361d830fa93da05ebfff1afc1
Sourabh Kulkarni <sourabhk2887@gmail.com>
2023-11-23 19:45:45 +0530

    Made a few changes in the drop down menu & it's button

--- super_admin/src/components/NewGroupForms/Drop Down Menu/CustomDropdown.js
+++ super_admin/src/components/NewGroupForms/Drop Down Menu/CustomDropdown.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import dropDownStyles from './DropDownStyle'; // Import the new dropDownStyles
+import dropDownStyles from './DropDownStyle';
+import dropDownData from './DropDown.json';
 
 const CustomDropdown = () => {
+    const options = dropDownData
+        .filter(item => !item.isButton) // Exclude the "Add App" item
+        .map(item => {
+            return (
+                <option
+                    key={item.id}
+                    value={item.name}
+                    disabled={item.isDisabled}
+                >
+                    {item.name}
+                </option>
+            );
+        });
+
+    const addButtonLabel = dropDownData.find(item => item.isButton).name;
+
     return (
         <div className="flex items-center">
             <div className={`relative flex-initial w-96 mr-4 ${dropDownStyles.select.border}`}>
@@ -10,13 +27,11 @@ const CustomDropdown = () => {
                     id="selectedApp"
                     className={`${dropDownStyles.select.base} ${dropDownStyles.select.placeholder}`}
                 >
-                    <option value="" disabled defaultValue>
-                        Select apps to add to the group
-                    </option>
+                    {options}
                 </select>
             </div>
             <button type="button" className={dropDownStyles.addButton.base}>
-                Add App
+                {addButtonLabel}
             </button>
         </div>
     );

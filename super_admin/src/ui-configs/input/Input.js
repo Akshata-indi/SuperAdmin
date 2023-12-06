edb70cbb97944cd7cb3d80b24787f69db85abd6c
Akashata-indi <indi.basamma@infokalash.com>
2023-12-06 11:44:06 +0530

    updated input folder and table folder

--- super_admin/src/ui-configs/input/Input.js
+++ super_admin/src/ui-configs/input/Input.js
@@ -1,36 +1,12 @@
-<<<<<<< HEAD
-import React from 'react';
-import InputComponent from './InputComponent';
-import InputStyle from './InputStyle';
-
-
-const Input = ({ config }) => {
-  const handleChange = (e) => {
-    // Implement your logic to handle input changes here
-    console.log(e.target.value);
-  };
-
-  return (
-    <div>
-      {config.map((input, index) => (
-        <InputComponent
-          key={index}
-          label={input.label}
-          value={''} // Provide appropriate value based on your state or props
-          onChange={handleChange} // Pass your change handler function
-          textcss={InputStyle[input.textcss]}
-        />
-      ))}
-=======
 import React from 'react'
 
 import DynamicInput from './DynamicInput'
 
-export const Input = ({inputConfigs}) => {
+export const Input = ({ inputConfigs }) => {
   return (
     <div>
-        <DynamicInput config={inputConfigs} />
->>>>>>> Master
+      <DynamicInput config={inputConfigs} />
+
     </div>
   );
 };

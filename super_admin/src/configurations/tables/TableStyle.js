791461a0414cf79d3a5c76d9526f6ab72f7ad66d
Ushamnaik999 <naik.usha@infokalash.com>
2023-12-12 17:54:32 +0530

    implementing adduser  Forms

--- super_admin/src/configurations/tables/TableStyle.js
+++ super_admin/src/configurations/tables/TableStyle.js
@@ -2,9 +2,9 @@ const TableStyle = {
     
   
 
-  style1: "   text-center  px-10  py-1 border-b font-bold text-sm",
+  style1: "   text-center  px-10 py-2 border-b font-bold  text-gray-600 text-sm",
 
-  datastyle: "   border-b text-center text-gray-800 px-10  py-1 text-sm "
+  datastyle: "   border-b text-center text-gray-800 px-10  py-4 text-sm "
  
   
   }

ffee6abde27c74c00cdce2cda627aa59609a183d
iramsaba-m <mirjankar.iramsaba@infokalash.com>
2023-12-15 12:41:07 +0530

    form style changes

--- super_admin/src/configurations/form_modal/formModalStyle.js
+++ super_admin/src/configurations/form_modal/formModalStyle.js
@@ -3,7 +3,7 @@ const formModalStyles = {
     overlay: 'fixed inset-0 flex items-center justify-center z-20',
     overlayBackground: 'ModalComponent-overlay fixed inset-0 bg-black opacity-10',
     container: 'ModalComponent-container bg-white p-10 W-96 rounded-lg shadow-md relative z-10',
-    title: 'text-xl font-semibold p-10 mb-2',
+    title: 'text-xl font-semibold p-2 mb-1',
     content: 'text-gray-600',
     createbutton: 'px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600',
     closeButton:'px-4 py-2 text-black bg-white rounded-md border-2 hover:bg-gray-300'

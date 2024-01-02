80adc469241e7eb90d6bc130b4be28eb1479c74e
Akashata-indi <indi.basamma@infokalash.com>
2024-01-02 16:24:08 +0530

    app file updated

--- super_admin/src/App.js
+++ super_admin/src/App.js
@@ -1,41 +1,26 @@
-
-
-import Role from "./components/RoleForms/Body/Role";
-
 import { Outlet, Route, Routes } from "react-router-dom";
 import routesConfig from './routing/RoutingConfig'
 import StyleProvider from "./components/SidebarMenus/menusContext/StyleProvider";
-
-
 // import Layout from "./components/layout/Layout";
-
 import Layout from "./components/layout/Layout";
-import RoleComponent from "./pages/roles/RoleComponent";
-import UserComponent from "./pages/userContent/UserComponent";
-import UserForm from "./pages/userContent/UserForm";
 // import TableWithData from "./configurations/tables/TableWithData";
-
-
-import Groups from './pages/groups/Groups'
-
-function App()  {
+function App() {
+  const renderRoutes = (routes) => {
+    return routes.map((route) => (
+      <Route key={route.path} path={route.path} element={route.element} index={route.index}>
+        {route.children && renderRoutes(route.children)}
+      </Route>
+    ))
+  };
   return (
-    <div>
-      <Layout/>
-      {/* <TableWithData /> */}
-    {/* <UserForm/> */}
-    {/* <UserComponent /> */}
-    {/* <Role /> */}
-    {/* <UserHeader /> */}
-    {/* <DSearch /> */}
-    {/* <Groups /> */}
-    </div>
-    
-
-
-
- 
+    <>
+      <div>
+        <StyleProvider>
+          <Routes>{renderRoutes(routesConfig)}</Routes>
+        </StyleProvider>
+        <Outlet />
+      </div>
+    </>
   );
 }
-
 export default App;
